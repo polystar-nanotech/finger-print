8f3b7db726c41ef692b14a2338cb83248214ba78
sauverpro <unsauvechriss@gmail.com>
2025-01-03 13:58:00 +0200

    modifying json request

--- src/services/enrollmentService.js
+++ src/services/enrollmentService.js
@@ -15,7 +15,7 @@ export const enrollFingerprint = async (fingerprintId) => {
   try {
     // Send a POST request to the hardware's enrollment endpoint
     const response = await axios.post(`${HARDWARE_API_URL}/enroll`, {
-      fingerprint_id: fingerprintId,
+      ID: fingerprintId,
     });
 
     // Check response status
@@ -32,6 +32,33 @@ export const enrollFingerprint = async (fingerprintId) => {
     console.error(
       `Error communicating with hardware for Fingerprint ID: ${fingerprintId}`
     );
+    console.error(error);
+    console.error(error.message);
+    return false;
+  }
+};
+export const verifyFingerprint = async (fingerprintId) => {
+  try {
+    // Send a POST request to the hardware's enrollment endpoint
+    const response = await axios.post(`${HARDWARE_API_URL}/enroll`, {
+      message: "request",
+    });
+
+    // Check response status
+    if (response.data && response.data.status === "success") {
+      console.log(`Enrollment successful for Fingerprint ID: ${fingerprintId}`);
+      return true;
+    } else {
+      console.error(`Enrollment failed for Fingerprint ID: ${fingerprintId}`);
+      console.error(`Reason: ${response.data.message || "Unknown error"}`);
+      return false;
+    }
+  } catch (error) {
+    // Handle errors from the hardware API
+    console.error(
+      `Error communicating with hardware for Fingerprint ID: ${fingerprintId}`
+    );
+    console.error(error);
     console.error(error.message);
     return false;
   }
